4e41df9c58ced3209b37d1aa35e21068f3fcd30a
AlexeyLozovoy <gvozdik@ua.fm>
2023-01-23 13:44:32 +0200

    Kvadrat

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Создать классы фигура, прямоугольник, квадрат, круг и эллипс.
 # Реализовать методы вычисления площади
+
+class Kvadrat:
+    def __init__(self, Storona):
+        self.Storona = Storona
+        pkv = Storona * Storona
+        print("Площадь Квадрата: ", pkv)
+
+kv = float(input("Ведите сторону квадрата: "))
+pkv = Kvadrat(kv)
